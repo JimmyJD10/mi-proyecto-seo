45043d337e615cad63caef47f8f23ea4e614b94b
Jimmy Ventura <jimmy.ventura@tecsup.edu.pe>
2025-06-18 16:34:24 -0500

    Primer commit

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -2,6 +2,7 @@ const BASE_URL = "https://mi-proyecto-seo-sooty.vercel.app";
 
 // Simulación de consulta a base de datos o CMS
 async function getDynamicRoutes() {
+  // Aquí podrías consultar una base de datos real
   return [
     "/",
     "/blog",
@@ -14,13 +15,12 @@ export default async function handler(req, res) {
   const urls = await getDynamicRoutes();
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
-      .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
-      .join("")}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls
+    .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
+    .join("")}
+</urlset>`;
 
   res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
+  res.status(200).end(sitemap);
 }
