7aa068c70a144e06defb13cac13b1148ac84872c
Jimmy Ventura <jimmy.ventura@tecsup.edu.pe>
2025-06-18 14:32:49 -0500

    Modificacion

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,28 +1,25 @@
-const BASE_URL = "https://mi-proyecto-seo-git-master-jimmyjd10s-projects.vercel.app";
+const BASE_URL = "https://mi-proyecto-seo-sooty.vercel.app";
 
-
-// Simulación de consulta a base de datos o CMS
 async function getDynamicRoutes() {
-  // Aquí podrías consultar una base de datos real
   return [
     "/",
     "/blog",
     "/contacto",
     "/blog/articulo-seo",
     "/blog/optimizacion-nextjs",
+    "/servicios",
   ];
 }
+
 export default async function handler(req, res) {
   const urls = await getDynamicRoutes();
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
-      .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
-      .join("")}
-  </urlset>`;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n` +
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
+    urls.map(url => `<url><loc>${BASE_URL}${url}</loc></url>`).join("\n") +
+    `\n</urlset>`;
 
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/xml");
+  res.end(sitemap);
 }
